43c641774025dbe02db3a62e3173328f538de9b7
Mostafa Hamdi <mostafa_hamdi235@outlook.com>
2025-02-03 19:57:30 +0200

    Update front.blade.php

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -159,10 +159,10 @@
                             </a>
                             <ul class="dropdown-menu">
                                 @foreach ($categories as $category)
-                                    <li><a class="dropdown-item nav-link dropdown-toggle" href="javascript();">{{ $category->{'title_' . $sign} }}</a>
+                                    <li><a class="dropdown-item nav-link dropdown-toggle" href="#">{{ $category->{'title_' . $sign} }}</a>
                                       <ul class="dropdown-menu">
                                         @foreach ($categories as $category)
-                                            <li><a class="dropdown-item" href="javascript();">{{ $category->{'title_' . $sign} }}</a>
+                                            <li><a class="dropdown-item" href="#">{{ $category->{'title_' . $sign} }}</a>
                                               
                                             </li>
                                         @endforeach
