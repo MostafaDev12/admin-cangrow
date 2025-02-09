a82e0d129e5d0ef46f043f7491d0d7ab0b465084
Mostafa Hamdi <mostafa_hamdi235@outlook.com>
2025-02-09 13:32:52 +0200

    Update details-blog.blade.php

--- resources/views/front/details-blog.blade.php
+++ resources/views/front/details-blog.blade.php
@@ -12,7 +12,11 @@
 
 
 @section('content')
-
+@php
+$phones =  explode(',', $gs->phones);
+ 
+$randomPhone = Arr::random($phones);
+@endphp
     <div class="header-title ">
         <div class="overlay d-flex justify-content-center align-items-center">
             <h1> {{ $blog->{'title_' . $sign} }}   </h1>
@@ -27,34 +31,7 @@
                         <img class="mb-4" width="100%" src="{{ $blog->photo }}" alt="">
                         
                         <p>   {!! $blog->{'details_' . $sign} !!}   </p>
-                        <div class="social-connect">
-                            <div class="container">
-                                <div class="row px-2">
-                                    <div class="col-6 col-md-6 col-lg-4 mb-2 mb-lg-0">
-                                        <a href="" target="_blank" class="btn-custom btn-responsive-action">
-                                            <div class="icon-container d-flex pt-3">
-                                                <i class="fab fa-whatsapp"></i>
-                                                <p class="color-white-important"> WhatsApp</p>
-                                               
-                                            </div>
-                                        </a>
-                                    </div>
-
-                                    <div class="col-6 col-md-6 col-lg-4 mb-2 mb-lg-0">
-                                        <a href="tel:+201118886541" class="btn-custom btn-responsive-action bg-2">
-                                            <div class="icon-container  d-flex pt-3">
-                                                <i class="fas fa-phone-alt"></i>
-                                                <p class="color-white-important"> Call Us</p>
-                                                
-                                            </div>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-
-
-                        </div>
-                        
+                     
                     </div>
                 </div>
                 <div class="col-12 col-lg-4 col-md-6 box">
