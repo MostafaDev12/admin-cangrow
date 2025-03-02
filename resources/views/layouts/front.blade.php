2934beb2d4055dda1d942708b1e885e4e57bab76
Mostafa Hamdi <mostafa_hamdi235@outlook.com>
2025-03-02 14:13:55 +0200

    Update front.blade.php

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -73,7 +73,15 @@
   }
 </script>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-8RJGGYWDV7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-8RJGGYWDV7');
+</script>
 
     @yield('gsearch')
     <!-- Google Font -->
