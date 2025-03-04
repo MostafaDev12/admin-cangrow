49eb6f73459ef19da7892cb37195b341473a5a5f
Mostafa Hamdi <mostafa_hamdi235@outlook.com>
2025-03-04 15:32:37 +0200

    Update front.php

--- routes/front.php
+++ routes/front.php
@@ -35,7 +35,7 @@ Route::middleware(['IpLocation', 'FrontLanguages'])->group(function () {
 
 
         Route::get('/', [HomeController::class, 'index'])->name('front.index');
-        Route::get('/عن-الشركه', [HomeController::class, 'about'])->name('about.index');
+        Route::get('/about-us', [HomeController::class, 'about'])->name('about.index');
         Route::get('/services', [HomeController::class, 'services'])->name('services.index');
         Route::get('/projects', [HomeController::class, 'projects'])->name('projects.index');
 
@@ -43,11 +43,11 @@ Route::middleware(['IpLocation', 'FrontLanguages'])->group(function () {
 
         Route::get('/category/{slug}', [HomeController::class, 'blogsCategory'])->name('blogs-category.index');
 
-        Route::get('/فيديوهات', [HomeController::class, 'videos'])->name('videos.index');
+        Route::get('/videos', [HomeController::class, 'videos'])->name('videos.index');
         Route::get('/news', [HomeController::class, 'news'])->name('news.index');    
-        Route::get('/المقالات', [HomeController::class, 'blogs'])->name('blogs.index');    
+        Route::get('/blogs', [HomeController::class, 'blogs'])->name('blogs.index');    
         
-        Route::get('/اتصل-بنا', [HomeController::class, 'contact'])->name('contact.index');
+        Route::get('/contact-us', [HomeController::class, 'contact'])->name('contact.index');
         Route::post('/contact-submit', [HomeController::class, 'contactemail'])->name('front.contact.submit');
         Route::get('/احجز-الان', [HomeController::class, 'BookNow'])->name('book.index');
 
