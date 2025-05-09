49ceeef6964f83a0b9322274bc2be8d5bef4c772
Mostafa Hamdi <mostafa_hamdi235@outlook.com>
2025-05-09 11:35:15 +0300

    Update index.blade.php

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -148,140 +148,124 @@ $randomPhone = Arr::random($phones);
     <section class="container mt-10 mx-auto px-4 md:px-8 lg:px-16 xl:px-32">
         <div class="text-center">
             <div><span class="uppercase text-accent font-semibold leading-4"></span>
-                <h2 class="text-primary font-bold text-4xl italic">المقالات</h2>
+                <h2 class="text-primary font-bold text-4xl italic">{{ __('المقالات') }}</h2>
             </div>
         </div>
         <div class="flex justify-between my-4">
-            <div><span class="uppercase text-accent font-semibold leading-4">المقالات</span>
+            <div><span class="uppercase text-accent font-semibold leading-4">{{ __('المقالات') }}</span>
                 <h2 class="text-primary font-bold text-4xl italic"></h2>
             </div><a
                 class="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:size-4 [&amp;_svg]:shrink-0 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 "
-                href="/blogs.html">عرض المزيد</a>
+                href="{{ route('blogs.index') }}"> {{ __('اعرف المزيد') }}  </a>
         </div>
         <div class="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+           
+            @foreach($blogs as $blog)
             <div class="rounded shadow hover:shadow-lg p-4 transition-all duration-500">
-                <a class="flex flex-col justify-start" href="/blog.deatils.html">
+                <a class="flex flex-col justify-start" href="{{ route('single-blog.index',$blog->{'slug_' . $sign}) }}">
                     <img class="rounded max-w-full h-auto object-cover aspect-video mb-4"
-                        src="https://cdn.pixabay.com/photo/2024/02/21/14/53/ai-generated-8587845_640.png"
-                        srcset="https://cdn.pixabay.com/photo/2024/02/21/14/53/ai-generated-8587845_640.png 1x, https://cdn.pixabay.com/photo/2024/02/21/14/53/ai-generated-8587845_1280.png 2x"
+                        src="{{ $blog->photo }}"
+                        srcset="{{ $blog->photo }} 1x, {{ $blog->photo }} 2x"
                         alt="Free Ai Generated Employee illustration and picture"
                         title="Download free HD stock image of Ai Generated Employee">
-                    <h2 class="text-xl mt-2 font-bold text-gray-800 mb-2 line-clamp-2">الرحاب لتوريد العمالة</h2>
+                    <h2 class="text-xl mt-2 font-bold text-gray-800 mb-2 line-clamp-2">  {{ $blog->{'title_' . $sign} }}     </h2>
                     <p class="text-gray-600 text-ellipsis overflow-hidden whitespace-nowrap max-w-full ">
-                        الرحاب لتوريد العمالة توفر بودى جارد، وحراسات خاصة للافراد والاسر والمجموعات السياحية ورجال
-                        الاعمال -- 01104891929
+                        {{ $blog->{'short_details_' . $sign} }}
                     </p>
                 </a>
             </div>
-            <div class="rounded shadow hover:shadow-lg p-4 transition-all duration-500">
-                <a class="flex flex-col justify-start" href="/blog.deatils.html">
-                    <img class="rounded max-w-full h-auto object-cover aspect-video mb-4"
-                        src="https://cdn.pixabay.com/photo/2024/02/21/14/53/ai-generated-8587845_640.png"
-                        srcset="https://cdn.pixabay.com/photo/2024/02/21/14/53/ai-generated-8587845_640.png 1x, https://cdn.pixabay.com/photo/2024/02/21/14/53/ai-generated-8587845_1280.png 2x"
-                        alt="Free Ai Generated Employee illustration and picture"
-                        title="Download free HD stock image of Ai Generated Employee">
-                    <h2 class="text-xl mt-2 font-bold text-gray-800 mb-2 line-clamp-2">الرحاب لتوريد العمالة</h2>
-                    <p class="text-gray-600 text-ellipsis overflow-hidden whitespace-nowrap max-w-full ">
-                        الرحاب لتوريد العمالة توفر بودى جارد، وحراسات خاصة للافراد والاسر والمجموعات السياحية ورجال
-                        الاعمال -- 01104891929
-                    </p>
-                </a>
-            </div>
-            <div class="rounded shadow hover:shadow-lg p-4 transition-all duration-500">
-                <a class="flex flex-col justify-start" href="/blog.deatils.html">
-                    <img class="rounded max-w-full h-auto object-cover aspect-video mb-4"
-                        src="https://cdn.pixabay.com/photo/2024/02/21/14/53/ai-generated-8587845_640.png"
-                        srcset="https://cdn.pixabay.com/photo/2024/02/21/14/53/ai-generated-8587845_640.png 1x, https://cdn.pixabay.com/photo/2024/02/21/14/53/ai-generated-8587845_1280.png 2x"
-                        alt="Free Ai Generated Employee illustration and picture"
-                        title="Download free HD stock image of Ai Generated Employee">
-                    <h2 class="text-xl mt-2 font-bold text-gray-800 mb-2 line-clamp-2">الرحاب لتوريد العمالة</h2>
-                    <p class="text-gray-600 text-ellipsis overflow-hidden whitespace-nowrap max-w-full ">
-                        الرحاب لتوريد العمالة توفر بودى جارد، وحراسات خاصة للافراد والاسر والمجموعات السياحية ورجال
-                        الاعمال -- 01104891929
-                    </p>
-                </a>
-            </div>
+            @endforeach
+            
+            
         </div>
     </section>
     <!-- blogs -->
     <!-- contact-us -->
+ 
+    @php
+$phones =  explode(',', $gs->phones);
+$emails =   explode(',', $gs->emails);
+$addresses =  json_decode($gs->{'addresses_' . $sign});
+$randomPhone = Arr::random($phones);
+$email = Arr::random($emails);
+@endphp
+
+    <!-- contact-us -->
     <section class="container my-10 mx-auto px-4 md:px-8 lg:px-16 xl:px-32">
         <div class="">
             <div class="text-center mb-4">
-                <div><span class="uppercase text-accent font-semibold leading-4">لا
-                        تتردد</span>
-                    <h2 class="text-primary font-bold text-4xl italic">اتصل بنا</h2>
+                <div><span class="uppercase text-accent font-semibold leading-4"> 
+                    {{ __('لا تتردد') }} </span>
+                    <h2 class="text-primary font-bold text-4xl italic">  {{ __('اتصل بنا') }}  </h2>
                 </div>
             </div>
             <div class="container">
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <div id="rightToLeft">
-                        <h2 class="font-semibold uppercase">اتصل بنا</h2>
+                        <h2 class="font-semibold uppercase">    {{ __('اتصل بنا') }} </h2>
                         <ul class="flex gap-4 my-4">
-                            <li
+                            @if ($email)
+                              <li
                                 style="background-color:#EA4335; width:3rem; height:3rem; border-radius:9999px; box-shadow:0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06); display:flex; justify-content:center; align-items:center;">
                                 <a target="_blank" class="flex justify-center items-center"
-                                    href="mailto:info@alrehab-eg.com">
+                                    href="mailto:{{ $email }}">
                                     <i class="fas fa-envelope text-white text-xl"></i>
                                 </a>
-                            </li>
+                            </li>  
+                            @endif
+                            @foreach ($phones as $phone)
+                               
                             <li
                                 style="background-color:#25D366; width:3rem; height:3rem; border-radius:9999px; box-shadow:0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06); display:flex; justify-content:center; align-items:center;">
                                 <a target="_blank" class="flex justify-center items-center"
-                                    href="https://wa.me/+201070006618">
+                                    href="https://wa.me/+2{{ $phone }}">
                                     <i class="fas fa-phone-volume text-white text-xl"></i>
                                 </a>
                             </li>
-                            <li
-                                style="background-color:#25D366; width:3rem; height:3rem; border-radius:9999px; box-shadow:0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06); display:flex; justify-content:center; align-items:center;">
-                                <a target="_blank" class="flex justify-center items-center"
-                                    href="https://wa.me/+201070006618">
-                                    <i class="fas fa-phone-volume text-white text-xl"></i>
-                                </a>
-                            </li>
-                            <li
-                                style="background-color:#25D366; width:3rem; height:3rem; border-radius:9999px; box-shadow:0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06); display:flex; justify-content:center; align-items:center;">
-                                <a target="_blank" class="flex justify-center items-center"
-                                    href="https://wa.me/+201070006618">
-                                    <i class="fas fa-phone-volume text-white text-xl"></i>
-                                </a>
-                            </li>
+                         
+                            @endforeach
+                            @if ($randomPhone)
                             <li
                                 style="background-color:#FF9900; width:3rem; height:3rem; border-radius:9999px; box-shadow:0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06); display:flex; justify-content:center; align-items:center;">
-                                <a target="_blank" class="flex justify-center items-center" href="">
-                                    <i class="fas fa-location-dot text-white text-xl"></i>
+                                <a target="_blank" class="flex justify-center items-center" href="tel:+2{{ $randomPhone }}">
+                                    <i class="fas fa-phone-volume text-white text-xl"></i>
                                 </a>
                             </li>
+                            @endif
                         </ul>
                     </div>
                     <div id="leftToRight">
-                        <form>
-                            <div class="mb-4">
-                                <label for="الاسم" class="block text-sm font-semibold text-gray-700">الاسم</label>
-                                <input type="text" id="الاسم" placeholder="أدخل اسمك"
-                                    class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-primary"
-                                    required="" name="الاسم">
+                        <form action="{{route('front.contact.submit')}}" name="appointment" id="email-form" method="POST" autocomplete="off" class="cons-contact-form">
+                            {{csrf_field()}}
+                            <div class="form-group w-100">
+                              <div class="response w-100"></div>
                             </div>
                             <div class="mb-4">
-                                <label for="البريد الإلكتروني" class="block text-sm font-semibold text-gray-700">البريد
+                                <label for="name" class="block text-sm font-semibold text-gray-700">{{ __('الاسم') }}</label>
+                                <input type="text" id="name" placeholder="{{ __(key: 'أدخل اسمك') }}"
+                                    class="fname mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-primary"
+                                    required="" name="name">
+                            </div>
+                            <div class="mb-4">
+                                <label for="email" class="block text-sm font-semibold text-gray-700">البريد
                                     الإلكتروني</label>
-                                <input type="email" id="البريد الإلكتروني" placeholder="أدخل بريدك الإلكتروني"
+                                <input type="email" id="email" placeholder="أدخل بريدك الإلكتروني"
                                     class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-primary"
-                                    required="" name="البريد الإلكتروني">
+                                    required="" name="email">
                             </div>
                             <div class="mb-4">
-                                <label for="الموضوع" class="block text-sm font-semibold text-gray-700">الموضوع</label>
-                                <input type="text" id="الموضوع" placeholder="أدخل الموضوع"
+                                <label for="subject" class="block text-sm font-semibold text-gray-700">الموضوع</label>
+                                <input type="text" id="subject" placeholder="أدخل الموضوع"
                                     class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-primary"
-                                    required="" name="الموضوع">
+                                    required="" name="subject">
                             </div>
                             <div class="mb-4">
-                                <label for="الرسالة"
+                                <label for="text"
                                     class="block text-sm font-semibold text-gray-700">الرسالة</label><textarea
-                                    id="الرسالة" name="الرسالة" rows="4" placeholder="أدخل رسالتك"
+                                    id="text" name="text" rows="4" placeholder="أدخل رسالتك"
                                     class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-primary"
                                     required="">
-                            </textarea>
+                                </textarea>
                             </div>
                             <div class="text-center">
                                 <button type="submit"
@@ -294,34 +278,48 @@ $randomPhone = Arr::random($phones);
                     <div id="rightToLeft">
                         <h2 class="font-semibold uppercase">وسائل التواصل الاجتماعي</h2>
                         <ul class="flex gap-4 my-4">
+                            @if(App\Models\Socialsetting::find(1)->t_status == 1) 
                             <li
                                 style="background-color:#000; width:3rem; height:3rem; border-radius:9999px; box-shadow:0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06); display:flex; justify-content:center; align-items:center;">
                                 <a target="_blank" class="flex justify-center items-center"
-                                    href="https://twitter.com/2015_elre7ab">
+                                    href="{{ App\Models\Socialsetting::find(1)->twitter }}">
                                     <i class="fab fa-x-twitter text-white text-xl"></i>
                                 </a>
                             </li>
+                            @endif 
+                            @if(App\Models\Socialsetting::find(1)->ystatus == 1)
                             <li
                                 style="background-color:#FF0000; width:3rem; height:3rem; border-radius:9999px; box-shadow:0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06); display:flex; justify-content:center; align-items:center;">
                                 <a target="_blank" class="flex justify-center items-center"
-                                    href="https://www.youtube.com/channel/UCRxCrH5bl3gxSYlTZYRlBBg">
+                                    href="{{ App\Models\Socialsetting::find(1)->youtube }}">
                                     <i class="fab fa-youtube text-white text-xl"></i>
                                 </a>
                             </li>
+                            @endif  
+                            @if(App\Models\Socialsetting::find(1)->f_status == 1)       
                             <li
                                 style="background-color:#1877F2; width:3rem; height:3rem; border-radius:9999px; box-shadow:0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06); display:flex; justify-content:center; align-items:center;">
                                 <a target="_blank" class="flex justify-center items-center"
-                                    href="https://www.facebook.com/ElRehab4Services">
+                                    href="{{ App\Models\Socialsetting::find(1)->facebook }}">
                                     <i class="fab fa-facebook-f text-white text-xl"></i>
                                 </a>
                             </li>
+                            @endif  
                         </ul>
                     </div>
                     <div id="leftToRight"
-                        class="overflow-hidden rounded transition duration-500 shadow hover:shadow-lg"><iframe
+                        class="overflow-hidden rounded transition duration-500 shadow hover:shadow-lg">
+                        {{-- <iframe
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2819.130995559972!2d31.329487997527696!3d30.106386821254173!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1458159301bad8c5%3A0x82da1efc0d58130b!2z2YbYp9iv2Yog2KfZhNmG2LXYsSDYp9mE2LHZitin2LbZig!5e1!3m2!1sar!2seg!4v1741073226589!5m2!1sar!2seg"
                             class="w-full h-full" style="border:0;" allowfullscreen="" loading="lazy"
-                            referrerpolicy="no-referrer-when-downgrade"></iframe> </div>
+                            referrerpolicy="no-referrer-when-downgrade"></iframe> --}}
+                          
+
+                            <iframe
+                            src="  {!! $gs->map !!}"
+                            class="w-full h-full" style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
                 </div>
             </div>
         </div>
