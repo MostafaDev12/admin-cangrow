bf52521be81319a4db59d219e612be974f94c2b0
Mostafa Hamdi <mostafa_hamdi235@outlook.com>
2025-12-18 14:18:21 +0200

    aaa

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -27,7 +27,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./assets/style/index.css">
+    <link rel="stylesheet" href="{{ asset('front/dareltawfik/') }}/assets/style/index.css">
     <style>
 
     </style>
